bd7003687cf4b5d5c6b67a88e105690855b13d5f
anaysharma <anaysharma8791@gmail.com>
2022-06-03 11:14:45 +0530

    initial commit

---
+++ src/Pikotaro/PPAP.dart
@@ -0,0 +1,7 @@
+PPAP() {
+  var applePen = hold(pen) + hold(apple);
+  var pineapplePen = hold(pen) + hold(pineapple);
+  var penPineappleApplePen = applePen + pineapplePen;
+
+  return penPineappleApplePen;
+}
